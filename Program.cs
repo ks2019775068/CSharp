c52302e185c4556ce88e877a47e2273384211f8f
ks2019775068 <80227900+ks2019775068@users.noreply.github.com>
2021-03-08 10:58:50 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello CSharp world!!!");
+			//Console.ReadLine();
+			Console.WriteLine("2019775068 하수빈");
+		}
+	}
+}
