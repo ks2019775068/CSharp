5cf0495a00a5b4f6f8b92b7abe3704e2a4e53b9a
ks2019775068 <80227900+ks2019775068@users.noreply.github.com>
2021-03-15 10:36:16 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,14 +1,26 @@
 ﻿using System;
-
-namespace HelloWorld
+    class PropertyClass
+    {
+        private int privateValue;
+        public int PrivateValue
+        {
+            get { return privateValue; } // get-accessor
+            set { privateValue = value; } // set-accessor
+        }
+        public void PrintValue()
+        {
+            Console.WriteLine("Hidden Value = " + privateValue);
+        }
+    }
+class PropertyApp
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Hello CSharp world!!!");
-			//Console.ReadLine();
-			Console.WriteLine("2019775068 하수빈");
-		}
-	}
-}
+    public static void Main()
+    {
+        int n;
+        PropertyClass obj = new PropertyClass();
+        obj.PrivateValue = 100; // invoke set-accessor
+        obj.PrintValue();
+        n = obj.PrivateValue; // invoke get-accessor
+        Console.WriteLine(" Value = " + n);
+    }
+}
