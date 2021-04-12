b3bbdc6c32c66d6ba1a7cb24c78fdbaee518e735
ks2019775068 <80227900+ks2019775068@users.noreply.github.com>
2021-04-12 16:03:45 +0900

    Add files via upload

---
+++ 0412_C#/ch_9/DelegateExample.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace DelegateExample
+{
+    class DelegateExample
+    {
+        delegate bool MemberTest(int a);
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 3, 5, 4, 2, 6, 4, 6, 8, 54, 23, 4, 6, 4 };
+            Console.WriteLine(Count(4));
+            Console.WriteLine(Count(arr, 4));
+            Console.WriteLine("짝수의 갯수: " + EvenCount(arr));
+            Console.WriteLine("홀수의 갯수: " + OddCount(arr));
+            Console.WriteLine("짝수의 갯수: " + Count(arr, IsEven));
+            Console.WriteLine("홀수의 갯수: " + Count(arr, IsOdd));
+        }
+        static int EvenCount(int[] a)
+        {
+            int cnt = 0;
+            foreach (var n in a)
+            {
+                if (n % 2 == 0)
+                    cnt++;
+            }
+            return cnt;
+        }
+        static int OddCount(int[] a)
+        {
+            int cnt = 0;
+            foreach (var n in a)
+            {
+                if (n % 2 == 1)
+                    cnt++;
+            }
+            return cnt;
+        }
---
+++ 0412_C#/ch_9/EventHandlingApp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace EventHandlingApp
+{
+    public delegate void MyEventHandler();
+    class Button
+    {
+        public event MyEventHandler Push;
+        public void OnPush()
+        {
+            if (Push != null)
+                Push();
+        }
+    }
+    class EventHandlerClass
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("In the EventHandlerClass.MyMethod ...");
+        }
+    }
+    class EventHandlingApp
+    {
+        public static void Main()
+        {
+            Button button = new Button();
+            EventHandlerClass obj = new EventHandlerClass();
+            button.Push += new MyEventHandler(obj.MyMethod);
+            button.OnPush();
+        }
+    }
+}
