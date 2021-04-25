00dc5dc04e70ba5c3d22f0aaf773a6349dc01cdf
ks2019775068 <80227900+ks2019775068@users.noreply.github.com>
2021-04-26 08:51:27 +0900

    Add files via upload

---
+++ 0426_C#/DivByZeroExceptionApp1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace DivByZeroExceptionApp
+{
+    class DivByZeroExceptionApp
+    {
+        public static void Main()
+        {
+            int i = 1, j = 0, k;
+            k = i / j;
+        }
+    }
+}
---
+++ 0426_C#/IndexOutOfRangeApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace IndexOutOfRangeApp
+{
+    class IndexOutOfRangeApp
+    {
+        public static void Main()
+        {
+            int[] vector = { 1, 2, 3 };
+            vector[4] = 4;
+        }
+    }
+}
