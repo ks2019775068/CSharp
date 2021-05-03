8054677550d177a3f934cf3fc79d826fb6deb3a8
ks2019775068 <80227900+ks2019775068@users.noreply.github.com>
2021-05-03 09:56:35 +0900

    Add files via upload

---
+++ 0503_C#/ArrayList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayList
+{
+    class ArrayList
+    {
+        static void Main(string[] args)
+        {
+            ArrayList a = new ArrayList();
+            Random r = new Random();
+            PrintValues(a);
+            for (int i = 0; i < 10; i++)
+                a.Add(r.Next(100));
+            PrintValues(a);
+            a.Sort();
+            PrintValues(a);
+            a.RemoveAt(3);
+            PrintValues(a);
+        }
+
+        private void RemoveAt(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Sort()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Add(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void PrintValues(ArrayList a)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void PrintValues(global::ArrayList a)
+        {
+            Console.WriteLine("Print Values in ArrayList");
+            Console.WriteLine(" Count = {0}", a.Count);
+            Console.WriteLine(" Capacity = {0}", a.Capacity);
+            foreach (var i in a)
+                Console.Write(" {0}", i);
+            Console.WriteLine();
+        }
+    }
+}
